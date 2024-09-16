9be15d8f69bcb9456ed9a528c241e10b33c10405
Roberto Valezi Jr <117041697+Roberto-Valezi@users.noreply.github.com>
2024-09-16 07:38:43 -0300

    retirado comentarios

--- src/js/acordeon.js
+++ src/js/acordeon.js
@@ -9,19 +9,3 @@ itensPerguntasERespostas.forEach(function (item) {
         item.classList.add("ativo");
     });
 })
-
-
-
-
-/*
-const itensperguntasERespostas = document.querySelectorAll(".item");
-
-itensperguntasERespostas.forEach(function (item) {
-    item.addEventListener("click", function () {
-        const itemAtivoAtual = document.querySelector(".ativo");
-        itemAtivoAtual.classList.remove("ativo");
-        item.classList.add("ativo");
-    })
-})
-
-*/
